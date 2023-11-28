503269e1b128105ea6dd7a14cccc2b186ddb98f9
zhangdd <2067949563@qq.com>
2023-11-28 14:46:25 +0800

    brand api

---
+++ mall-admin/src/main/java/cnn/wwinter/malladmin/service/BrandService.java
@@ -0,0 +1,28 @@
+package cnn.wwinter.malladmin.service;
+
+import cn.wwinter.model.PmsBrand;
+import cnn.wwinter.malladmin.model.dto.PmsBrandDto;
+
+import java.util.List;
+
+/**
+ * ClassName: BrandService
+ * Package: cn.wwinter.malldemo.service
+ * Description:
+ * Datetime: 2023/11/27
+ * Author: zhangdd
+ */
+public interface BrandService {
+
+    PmsBrand getBrand(Long id);
+
+    List<PmsBrand> listBrands();
+
+    List<PmsBrand> listBrands(Integer pageNum, Integer pageSize);
+
+    int createBrand(PmsBrandDto pmsBrandDto);
+
+    int updateBrand(Long id, PmsBrandDto pmsBrandDto);
+
+    int deleteBrand(Long id);
+}
