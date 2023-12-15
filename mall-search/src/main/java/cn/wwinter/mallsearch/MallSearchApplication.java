3d24715411ffd315febf7db387501d7949fbea7e
zhangdd <2067949563@qq.com>
2023-12-15 09:13:44 +0800

    添加mall-search模块

---
+++ mall-search/src/main/java/cn/wwinter/mallsearch/MallSearchApplication.java
@@ -0,0 +1,13 @@
+package cn.wwinter.mallsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallSearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallSearchApplication.class, args);
+    }
+
+}
